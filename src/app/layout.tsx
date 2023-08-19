b87e8458f1d7370d893e04df27d48dca5b8c61b0
Borhan Saflo <60056206+BorhanSaflo@users.noreply.github.com>
2023-08-19 03:15:45 -0400

    feat: CSS Favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,6 +8,11 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "CSS Site",
   description: "CSS Site",
+  icons: {
+    icon: "/favicon.ico",
+    shortcut: "/favicon-16x16.png",
+    apple: "/apple-touch-icon.png",
+  },
 };
 
 export default function RootLayout({
