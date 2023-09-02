e0e4094ca5bc02d15701f0dddbe79b7900d54432
Gabriel Rueda <ruedag@uwindsor.ca>
2023-09-02 12:35:37 -0700

    Read from File Instead

--- src/app/(pages)/about/page.tsx
+++ src/app/(pages)/about/page.tsx
@@ -1,8 +1,7 @@
 import { Metadata } from "next";
 import React, { useEffect, useState } from 'react'
 import MarkDownView from "@/components/MarkDownView";
-import { prisma } from "@/lib/db";
-
+import fs from 'fs';
 
 export const metadata: Metadata = {
   title: "About",
@@ -10,16 +9,17 @@ export const metadata: Metadata = {
 
 
 export default function AboutPage() {
-  const data = prisma.plainText.findMany({
-    where: {id:1}
-  })
+  // const data = prisma.plainText.findMany({
+  //   where: {id:1}
+  // })
+  const markdownFile = fs.readFileSync('src/app/(pages)/about/content.mdx', 'utf-8');
 
   return (
     <>
       <h1 className="text-4xl text-center font-bold">About Us</h1>
         <MarkDownView
               className="prose dark:prose-invert max-w-none w-full break-words"
-              markdown={data[0].content}
+              markdown={markdownFile}
             />
     </>
   
