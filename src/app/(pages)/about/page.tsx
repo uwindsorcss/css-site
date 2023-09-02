23ca891d30ebc84a19c724b13cb4818fd1cf303c
Gabriel Rueda <ruedag@uwindsor.ca>
2023-09-02 08:43:29 -0700

    Added About Page Functionality

--- src/app/(pages)/about/page.tsx
+++ src/app/(pages)/about/page.tsx
@@ -1,13 +1,28 @@
 import { Metadata } from "next";
+import React, { useEffect, useState } from 'react'
+import MarkDownView from "@/components/MarkDownView";
+import { prisma } from "@/lib/db";
+
 
 export const metadata: Metadata = {
   title: "About",
 };
 
+
 export default function AboutPage() {
+  const data = prisma.plainText.findMany({
+    where: {id:1}
+  })
+
   return (
     <>
       <h1 className="text-4xl text-center font-bold">About Us</h1>
+        <MarkDownView
+              className="prose dark:prose-invert max-w-none w-full break-words"
+              markdown={data.content}
+            />
     </>
-  );
+  
+);
 }
+
