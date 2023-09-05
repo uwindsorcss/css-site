ec29e8a6a0626fa4399c0ffb3bf5384ef5b2b9e3
Borhan Saflo <60056206+BorhanSaflo@users.noreply.github.com>
2023-09-05 07:05:25 -0400

    feat: Check if User Is in the Server Before Linking or Unlinking

--- src/app/_actions.ts
+++ src/app/_actions.ts
@@ -18,17 +18,10 @@ export async function linkDiscordAccount(discordResponse: any) {
   try {
     const session = await getServerSession(authOptions);
 
-    if (!session?.user?.id) {
-      throw new Error("from linkDiscordAccount: no session found");
-    }
+    if (!session?.user?.id)
+      throw new Error("Error while linking account. Session not found.");
 
     const accessToken = discordResponse.access_token;
-
-    //translate discordResponse.expires_in to a date
-    const expiresAt = new Date();
-    expiresAt.setSeconds(
-      expiresAt.getSeconds() + discordResponse.expires_in - 60
-    );
 
     const discordUser = await fetch(`${DISCORD_API_ENDPOINT}/users/@me`, {
       headers: {
@@ -36,11 +29,11 @@ export async function linkDiscordAccount(discordResponse: any) {
       },
     }).then((res) => res.json());
 
-    const discordAccount = await prisma.discordAccount.findUnique({
-      where: {
-        userId: session?.user.id!,
-      },
-    });
+    //translate discordResponse.expires_in to a date
+    const expiresAt = new Date();
+    expiresAt.setSeconds(
+      expiresAt.getSeconds() + discordResponse.expires_in - 60
+    );
 
     const data = {
       discordId: discordUser.id,
@@ -52,95 +45,110 @@ export async function linkDiscordAccount(discordResponse: any) {
       expiresAt: expiresAt,
     };
 
-    if (discordAccount && discordAccount !== null) {
-      await prisma.discordAccount.update({
-        where: {
-          id: discordAccount.id,
-        },
-        data,
-      });
-    } else {
-      await prisma.discordAccount.create({
-        data,
-      });
-    }
+    await prisma.discordAccount.upsert({
+      where: {
+        userId: session?.user.id!,
+      },
+      update: data,
+      create: data,
+    });
 
-    // Add the user to the server
-    // await fetch(
-    //   `https://discord.com/api/guilds/${process.env.DISCORD_GUILD_ID}/members/${discordUser.id}`,
-    //   {
-    //     method: "PUT",
-    //     headers: {
-    //       Authorization: `Bot ${process.env.DISCORD_BOT_TOKEN}`,
-    //       "Content-Type": "application/json",
-    //     },
-    //     body: JSON.stringify({
-    //       access_token: accessToken,
-    //       nick: session?.user.name,
-    //     }),
-    //   }
-    // );
+    //check if the user is already in the server
+    const member = await getMemberFromServer(discordUser.id);
+
+    if (!member.user) {
+      // Add the user to the server
+      await fetch(
+        `https://discord.com/api/guilds/${process.env.DISCORD_GUILD_ID}/members/${discordUser.id}`,
+        {
+          method: "PUT",
+          headers: {
+            Authorization: `Bot ${process.env.DISCORD_BOT_TOKEN}`,
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            access_token: accessToken,
+            nick: session?.user.name,
+          }),
+        }
+      );
+    }
 
     // Send a confirmation DM to the user
     await sendDiscordDM(
       discordUser.id,
       `🔗✅ You've successfully linked your account.\n\n Welcome to the **University of Windsor CS Discord**! You've come to a great place.\n\n
-      We've set your nickname to **${session?.user.name}**. Please contact a CSS member if you'd like to shorten your name (e.g. Johnathon Middlename Doe -> John Doe).`
+            We've set your nickname to **${session?.user.name}**. Please contact a CSS member if you'd like to shorten your name (e.g. Johnathon Middlename Doe -> John Doe).`
     );
   } catch (error) {
     console.error("An error occurred:", error);
+    throw error;
   }
 }
 
 export async function unlinkDiscordAccount() {
-  const session = await getServerSession(authOptions);
-
-  if (!session?.user?.id) {
-    console.log("from unlinkDiscordAccount: no session found");
-    return false;
-  }
-
   try {
+    const session = await getServerSession(authOptions);
+
+    if (!session?.user?.id)
+      throw new Error("Error while unlinking account. Session not found.");
+
     const discordAccount = await prisma.discordAccount.findUnique({
       where: {
         userId: session?.user?.id!,
       },
     });
 
-    if (discordAccount) {
+    if (discordAccount && discordAccount !== null) {
+      // Check if the user is in the server
+      const member = await getMemberFromServer(discordAccount.discordId);
+
       await sendDiscordDM(
         discordAccount.discordId,
         `🔗💥 You've successfully unlinked your account.\n\n 
         You've been removed from the server. If you'd like to rejoin, please [relink](https://css.uwindsor.ca/discord) your account.`
       );
 
-      // Remove the user from the server
-      // await fetch(
-      //   `https://discord.com/api/guilds/${process.env.DISCORD_GUILD_ID}/members/${discordAccount.id}`,
-      //   {
-      //     method: "DELETE",
-      //     headers: {
-      //       Authorization: `Bot ${process.env.DISCORD_BOT_TOKEN}`,
-      //     },
-      //   }
-      // ).catch((error) => {
-      //   console.error("An error occurred:", error);
-      // });
+      if (member.user) {
+        // Remove the user from the server
+        await fetch(
+          `https://discord.com/api/guilds/${process.env.DISCORD_GUILD_ID}/members/${discordAccount.id}`,
+          {
+            method: "DELETE",
+            headers: {
+              Authorization: `Bot ${process.env.DISCORD_BOT_TOKEN}`,
+            },
+          }
+        ).catch((error) => {
+          console.error("An error occurred:", error);
+        });
+      }
 
+      // Delete the discord account from the database
       await prisma.discordAccount.delete({
         where: {
           id: discordAccount.id,
         },
       });
+
       return true;
-    } else {
-      console.log("from unlinkDiscordAccount: no discord account found");
     }
-    return false;
+    throw new Error("No discord account found. Please try again.");
   } catch (error) {
     console.error("An error occurred:", error);
-    return false;
+    throw error;
   }
+}
+
+async function getMemberFromServer(userId: string) {
+  return await fetch(
+    `https://discord.com/api/guilds/${process.env.DISCORD_GUILD_ID}/members/${userId}`,
+    {
+      headers: {
+        Authorization: `Bot ${process.env.DISCORD_BOT_TOKEN}`,
+      },
+    }
+  ).then((res) => res.json());
 }
 
 export async function sendDiscordDM(userID: string, message: string) {
