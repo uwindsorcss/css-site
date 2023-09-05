eea005a85b0a0eb20838d4623dbcb2e6b05c1087
Borhan Saflo <60056206+BorhanSaflo@users.noreply.github.com>
2023-09-05 08:59:36 -0400

    update: Theme Color MetaData

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,7 +23,7 @@ export const metadata: Metadata = {
     shortcut: "/favicon-16x16.png",
     apple: "/apple-touch-icon.png",
   },
-  themeColor: "#07519D",
+  themeColor: "#0b0d14",
   openGraph: {
     type: "website",
     url: "https://css.uwindsor.ca",
