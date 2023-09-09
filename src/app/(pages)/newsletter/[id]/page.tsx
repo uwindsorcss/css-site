cd7aecd6aca1a00bfcaab4ed9b3350053b661f99
Borhan Saflo <60056206+BorhanSaflo@users.noreply.github.com>
2023-09-08 20:31:52 -0400

    feat: Generate Title Meta Tag for Newsletter Posts

--- src/app/(pages)/newsletter/[id]/page.tsx
+++ src/app/(pages)/newsletter/[id]/page.tsx
@@ -3,8 +3,27 @@ import MarkDownView from "@/components/views/MarkDownView";
 import BackButton from "@/components/ui/back-button";
 import { prisma } from "@/lib/db";
 import { formatShortenedTimeDistance } from "@/lib/utils";
+import { Metadata } from "next";
 interface pageProps {
   params: { id: string };
+}
+
+//generate meta tags
+export async function generateMetadata(
+  { params }: pageProps
+): Promise<Metadata> {
+  const post = await prisma.post.findUnique({
+    where: {
+      id: parseInt(params.id),
+    },
+    select: {
+      title: true,
+    },
+  });
+
+  return {
+    title: post?.title,
+  }
 }
 
 export default async function Post({ params }: pageProps) {
