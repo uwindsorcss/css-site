019b7b35506996088aeb02acb1eefbd4bc9ea6be
Borhan Saflo <60056206+BorhanSaflo@users.noreply.github.com>
2023-10-03 23:57:09 -0400

    style/refactor: New Event Label

--- src/components/events/list-view/EventCard.tsx
+++ src/components/events/list-view/EventCard.tsx
@@ -11,28 +11,29 @@ interface EventCardProps {
 }
 
 function EventCard({ event, currentPage, filter }: EventCardProps) {
+
   function checkNew(createdAt: Date) {
     const currentDate = new Date();
     const currentUtcDate = new Date(currentDate.toUTCString());
     const millisecondsIn3Days = 3 * 24 * 60 * 60 * 1000;
     const timeDifference = Math.abs(createdAt.getTime() - currentUtcDate.getTime());
-    if (timeDifference <= millisecondsIn3Days) {
-      return true
-    }
-    return false
-  
+    return timeDifference <= millisecondsIn3Days;
   }
-  
+
   return (
     <Card className="hover:bg-gray-200 dark:hover:bg-card/50 transition-colors duration-300">
       <Link
         key={event.id}
         href={`/events/${event.id}?page=${currentPage}${filter ? `&filter=${filter}` : ""}`}>
-          {checkNew(event.createdAt) ?(
-            <div className="float-right m-5 p-2 rounded-full bg-cyan-600 text-white">NEW</div>
-          ):null}
+        {checkNew(event.createdAt) && (
+          <div className="relative w-full pb-1">
+            <div className="absolute top-0 right-0 bg-indigo-500 text-white text-xs font-bold px-2 py-1 rounded-bl rounded-tr">
+              New
+            </div>
+          </div>
+        )}
         <CardHeader>
-          <CardTitle >{event.title} </CardTitle>
+          <CardTitle>{event.title} </CardTitle>
           <CardDescription className="font-medium mb-2">
             {formatDateRange(event.startDate, event.endDate)} (
             {getEventRelativeTime(event.startDate, event.endDate)})
