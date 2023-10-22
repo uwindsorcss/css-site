8e2bb1aa6b391d8f27ab5a8d4a38c963e20577da
Borhan Saflo <60056206+BorhanSaflo@users.noreply.github.com>
2023-10-21 23:30:16 -0400

    refactor: Newsletter Post Page

--- src/app/(pages)/newsletter/[id]/page.tsx
+++ src/app/(pages)/newsletter/[id]/page.tsx
@@ -1,46 +1,47 @@
+import { Metadata } from "next";
 import FeedView from "@/components/views/FeedView";
 import MarkDownView from "@/components/views/MarkDownView";
 import BackButton from "@/components/ui/back-button";
-import { prisma } from "@/lib/db";
-import { formatTimeDifference, getSession, isModOrAdmin } from "@/lib/utils";
-import { Metadata } from "next";
 import EditPostButton from "@/components/newsletter/newsletter-post/EditPostButton";
 import DeletePostButton from "@/components/newsletter/newsletter-post/DeletePostButton";
-interface pageProps {
+import { prisma } from "@/lib/db";
+import { formatTimeDifference, getSession, isModOrAdmin } from "@/lib/utils";
+
+interface PageProps {
   params: { id: string };
 }
 
-//generate meta tags
-export async function generateMetadata({ params }: pageProps): Promise<Metadata> {
-  const post = await prisma.post.findUnique({
-    where: {
-      id: parseInt(params.id),
-    },
-    select: {
-      title: true,
-    },
+async function fetchPostById(id: string) {
+  return await prisma.post.findUnique({
+    where: { id: parseInt(id) },
+    include: { author: true },
   });
+}
 
+export async function generateMetadata({ params }: PageProps): Promise<Metadata> {
+  const post = await fetchPostById(params.id);
+  if (!post) return { title: "Post Not Found" };
   return {
     title: post?.title,
   };
 }
 
-export default async function Post({ params }: pageProps) {
+export default async function Post({ params }: PageProps) {
   const session = await getSession();
-  const post = await prisma.post.findUnique({
-    where: {
-      id: parseInt(params.id),
-    },
-    include: {
-      author: true,
-    },
-  });
+  const post = await fetchPostById(params.id);
+  if (!post) {
+    return (
+      <div className="text-center font-bold text-2xl mt-10 text-muted-foreground">
+        Post Not Found
+      </div>
+    );
+  }
+
+  const authorName = post?.author?.name ?? "CSS Team";
+  const formattedTime = formatTimeDifference(post!.createdAt);
 
   return (
-    <FeedView
-      heading={post?.title}
-      subheading={`${post?.author?.name ?? "CSS Team"} ● ${formatTimeDifference(post!.createdAt)}`}>
+    <FeedView heading={post?.title} subheading={`${authorName} ● ${formattedTime}`}>
       <MarkDownView allowLinks markdown={post!.content} />
       <div className="flex justify-between w-full mt-10">
         <BackButton href="/newsletter" />
