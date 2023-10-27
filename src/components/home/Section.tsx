426246bea7e96cfda3a6c3e6475e2ca76f6e1108
Borhan Saflo <60056206+BorhanSaflo@users.noreply.github.com>
2023-10-26 21:37:22 -0400

    style: Decrease Section Gap in Homepage

--- src/components/home/Section.tsx
+++ src/components/home/Section.tsx
@@ -10,7 +10,7 @@ interface HeroProps {
 function Hero({ className, children, heading, subheading }: HeroProps) {
   return (
     <section className={clsx("min-h-[50vh] my-12", className)}>
-      <div className="mx-auto flex flex-col items-center gap-12 py-8 px-4 max-w-7xl mb-20 w-full">
+      <div className="mx-auto flex flex-col items-center gap-8 py-8 px-4 max-w-7xl mb-20 w-full">
         <div className="max-w-xl text-center">
           {heading && (
             <h2 className="text-3xl md:text-3xl lg:text-4xl font-bold mb-5">{heading}</h2>
