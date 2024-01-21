76de283a586bb5bd43c33615f8d86b7596766965
Borhan Saflo <60056206+BorhanSaflo@users.noreply.github.com>
2024-01-21 01:33:21 -0500

    update: Dependencies

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,6 +6,7 @@ import NavBar from "@/components/navbar/Navbar";
 import { Toaster } from "@/components/ui/toaster";
 import NextTopLoader from "nextjs-toploader";
 import { metadataObject, viewportObject } from "./metadata";
+import { Suspense } from "react";
 
 const inter = Inter({ subsets: ["latin"] });
 export const metadata = metadataObject;
@@ -15,21 +16,23 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Providers>
-          <Toaster />
-          <NextTopLoader
-            color="#F0F0F2"
-            easing="ease-in-out"
-            height={2}
-            shadow={false}
-            showSpinner={false}
-            crawl={false}
-            speed={400}
-          />
-          <NavBar />
-          {children}
-          <Footer />
-        </Providers>
+        <Suspense>
+          <Providers>
+            <Toaster />
+            <NextTopLoader
+              color="#F0F0F2"
+              easing="ease-in-out"
+              height={2}
+              shadow={false}
+              showSpinner={false}
+              crawl={false}
+              speed={400}
+            />
+            <NavBar />
+            {children}
+            <Footer />
+          </Providers>
+        </Suspense>
       </body>
     </html>
   );
