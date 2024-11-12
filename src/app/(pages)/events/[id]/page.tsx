45dec5e9fc44ed6608901ffaa65fa6e7986286b7
Calder Johnson <calderbjohnson@icloud.com>
2024-11-11 21:57:13 -0500

    Fixed typo

--- src/app/(pages)/events/[id]/page.tsx
+++ src/app/(pages)/events/[id]/page.tsx
@@ -133,7 +133,7 @@ export default async function Post({ params, searchParams }: PageProps) {
           />
           <NextLink href={`/gallery/${event.id}`}>
             <Button>
-              View Gallary
+              View Gallery
             </Button>
           </NextLink>
           {session && canEditEvent(session) && (
