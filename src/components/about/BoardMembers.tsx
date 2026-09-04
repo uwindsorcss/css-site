3db295a1e27ead3fd680c08491357907e0a7e08e
Bovay <BroBovay@gmail.com>
2026-09-03 21:36:44 -0400

    feat: updated the 2026-2027 board

--- src/components/about/BoardMembers.tsx
+++ src/components/about/BoardMembers.tsx
@@ -2,7 +2,6 @@ import fs from "fs/promises";
 import path from "path";
 import Image from "next/image";
 import Anika from "../easter-eggs/Anika";
-import Marmot from "../easter-eggs/Marmot";
 
 interface Member {
   name: string;
@@ -12,7 +11,6 @@ interface Member {
 
 const EASTER_EGGS = {
   "Anika Khan": <Anika />,
-  "Ali Al Maamouri": <Marmot />,
 };
 
 async function BoardMembers() {
