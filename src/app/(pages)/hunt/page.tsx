f69cab103cf605eaf01edbb9af35e90d40010385
Bovay <BroBovay@gmail.com>
2026-09-07 17:14:53 -0400

    feat: updated scavenger hunt with submit link

--- src/app/(pages)/hunt/page.tsx
+++ src/app/(pages)/hunt/page.tsx
@@ -3,13 +3,15 @@ import MarkDownView from "@/components/views/MarkDownView";
 import { promises as fs } from "fs";
 import FeedView from "@/components/views/FeedView";
 import path from "path";
+import Button from '@mui/material/Button';
+
 
 export const revalidate = 3600;
 export const metadata: Metadata = {
   title: "Scavenger Hunt",
 };
 
-export default async function AboutPage() {
+export default async function HuntPage() {
   const markdownFile = await fs.readFile(
     path.join(process.cwd(), "src", "app", "(pages)", "hunt") + "/content.mdx",
     "utf8"
@@ -20,9 +22,19 @@ export default async function AboutPage() {
       <FeedView>
         <h1 className="mb-8 text-center text-4xl font-bold">Scavenger Hunt 2026</h1>
         <MarkDownView allowLinks markdown={markdownFile} />
+        <div style={{ marginTop: 8 }}>
+          <Button variant="contained" color="primary" href="https://forms.cloud.microsoft/r/fy4XwgPmDX">Join The Hunt!</Button>
+        </div>
+        {/* eslint-disable-next-line @next/next/no-img-element */}
+        <a href="/images/map.svg" target="_blank" rel="noopener noreferrer">
+           {/* eslint-disable-next-line @next/next/no-img-element */}
+           <img
+             src="/images/map.svg"
+             alt="Scavenger hunt campus map"
+             className="mt-4 h-auto w-full cursor-zoom-in rounded-lg border"
+           />
+         </a>
       </FeedView>
-      <div className="relative w-full">
-      </div>
     </>
   );
 }
